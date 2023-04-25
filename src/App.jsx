cfa86295c767873b396e4b534bfc0f8c9a82eaa4
Andrew Atimapre <atimaxandrew@gmail.com>
2023-04-25 11:59:35 +0100

    Ai Keyword Extractor Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>AI Keyword Extractor</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
